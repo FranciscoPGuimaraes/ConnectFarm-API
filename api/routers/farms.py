a6ab8435711741e1a2704fd01065ea61bf0fe566
Gabriel Siqueira <gabrielss2406@gmail.com>
2024-07-26 17:33:30 -0300

    feat: read farms from user

--- api/routers/farms.py
+++ api/routers/farms.py
@@ -1,11 +1,11 @@
 
-from typing import Dict
+from typing import Dict, List
 from uuid import UUID
 from fastapi import APIRouter, Depends, Security
 
 from api.dependencies import get_api_key, get_current_user, get_current_user_id
-from api.models.FarmModels import FarmIn
-from api.services.farm import create_farm, read_farm
+from api.models.FarmModels import FarmIn, FarmOut
+from api.services.farm import create_farm, read_farm, read_farms_from_user
 from fastapi import APIRouter, Depends
 
 from api.dependencies import get_api_key
@@ -27,10 +27,20 @@ async def register_new_user(farm: FarmIn, current_user_id: Dict = Depends(get_cu
         raise e
     
     
-@router.get("/{farm_id}")
-async def update_cattle_endpoint(farm_id: str):
+@router.get("/{farm_id}", response_model=FarmOut)
+async def get_farm_by_id(farm_id: str):
     try:
         result = await read_farm(farm_id)
+        return result
+    except Exception as e:
+        raise e
+    
+
+@router.get("/", response_model=List[FarmOut])
+async def get_farm_by_user(current_user_id: Dict = Depends(get_current_user_id)):
+    try:
+        print(current_user_id)
+        result = await read_farms_from_user(current_user_id)
         return result
     except Exception as e:
         raise e
--- api/services/farm/__init__.py
+++ api/services/farm/__init__.py
@@ -1,2 +1,2 @@
 from .create import create_farm
-from .read import read_farm
+from .read import read_farm, read_farms_from_user
