1d35649ccf2c15074e07d8887e48b550c544944f
Gabriel Siqueira <gabrielss2406@gmail.com>
2024-08-14 18:24:20 -0300

    feat: get all calves from a calf

--- api/services/farm/calf/read.py
+++ api/services/farm/calf/read.py
@@ -1,7 +1,8 @@
+from typing import List
 from uuid import UUID
 from fastapi import HTTPException
 from api.services.db import connect_mongo
-from api.models.CattleModels import Cattle
+from api.models.CattleModels import Calf, Cattle
 
 async def read_calf(farm_id: UUID, cattle_number: int, number: str):
     collection, client = connect_mongo("cattles")
@@ -13,3 +14,15 @@ async def read_calf(farm_id: UUID, cattle_number: int, number: str):
         return cattle["calves"][0]
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Erro ao ler calf: {e}")
+    
+
+async def read_calfs(farm_id: UUID, cattle_number: int) -> List[Calf]:
+    collection, client = connect_mongo("cattles")
+    try:
+        farm_id = str(farm_id)
+        cattle = collection.find_one({"farm_id": farm_id, "number": cattle_number}, {"calves": 1})
+        
+        if cattle and "calves" in cattle:
+            return cattle["calves"]
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Erro ao ler calfs: {e}")
