99da68972a9d10e3508fe8d5f8f589891b6fab8a
Gabriel Siqueira <gabrielss2406@gmail.com>
2024-09-20 19:45:25 -0300

    fix: vaccines quarter return

--- api/services/analysis/vaccines/vaccines_quarter.py
+++ api/services/analysis/vaccines/vaccines_quarter.py
@@ -1,7 +1,8 @@
 from uuid import UUID
-
 from fastapi import HTTPException
 from api.services.db.mongo_connection import connect_mongo
+
+PREDEFINED_VACCINE_TYPES = ["Brucelose", "Leucose Bovina", "Pneumonia Bovina", "Virose"]
 
 
 async def get_vaccinations_by_quarter(farm_id: UUID):
@@ -81,7 +82,29 @@ async def get_vaccinations_by_quarter(farm_id: UUID):
         # Executar a pipeline no MongoDB
         results = list(collection.aggregate(pipeline))
 
-        return results
+        # Formatar o resultado para incluir os tipos de vacina pré-definidos
+        formatted_results = []
+        for result in results:
+            formatted_result = {
+                "quadrimester": result["quadrimester"],
+                "total_vaccines": result["total_vaccines"],
+                "vaccine_types": {
+                    vaccine["type"]: vaccine["count"]
+                    for vaccine in result["vaccine_types"]
+                },
+            }
+            formatted_results.append(formatted_result)
+
+        # Adicionar tipos de vacina que não têm registros
+        for quadrimester in formatted_results:
+            for vaccine_type in PREDEFINED_VACCINE_TYPES:
+                if vaccine_type not in quadrimester["vaccine_types"]:
+                    quadrimester["vaccine_types"][vaccine_type] = 0
+
+        return {
+            "vaccination_data": formatted_results,
+            "vaccine_types": PREDEFINED_VACCINE_TYPES,
+        }
 
     except Exception as e:
         raise HTTPException(
