22d2e7c8abfdb0b4a30e3f9fdcb3c9c0799ad56f
Gabriel Siqueira <gabrielss2406@gmail.com>
2024-09-21 16:07:17 -0300

    fix: calf verify number

--- api/services/farm/calf/create.py
+++ api/services/farm/calf/create.py
@@ -3,21 +3,38 @@ from uuid import UUID
 from api.services.db import connect_mongo
 from api.models.CattleModels import CalfIn
 
+
 async def create_calf(farm_id: UUID, cattle_number: int, calf: CalfIn):
     collection, client = connect_mongo("cattles")
     try:
         farm_id = str(farm_id)
         calf_data = calf.dict(by_alias=True, exclude_unset=True)
-        
+
+        existing_calf = collection.find_one(
+            {
+                "farm_id": farm_id,
+                "calves": {"$elemMatch": {"number": calf_data["number"]}},
+            }
+        )
+
+        if existing_calf:
+            raise HTTPException(
+                status_code=400,
+                detail="Calf with this number already exists in the farm.",
+            )
+
         if "weights" in calf_data and isinstance(calf_data["weights"], dict):
             calf_data["weights"] = [calf_data["weights"]]
 
         result = collection.update_one(
             {"farm_id": farm_id, "number": cattle_number},
-            {"$push": {"calves": calf_data}}
+            {"$push": {"calves": calf_data}},
         )
+
         if result.matched_count == 0:
             raise HTTPException(status_code=404, detail="Cattle not found")
+
         return {"message": "Calf added successfully"}
+
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error adding calf: {e}")
